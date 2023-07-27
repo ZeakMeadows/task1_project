e4f591fa81dbb7cfa04ac48c4cca4ef5aa343bcc
Zeak Meadows <zeakmeadows@icloud.com>
2023-07-27 19:01:35 +0200

    Changed the output of the print function

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+# Before creating this file when running the 'git status' command it gave the following message in my terminal:
+
+# On branch master
+# No commits yet
+# nothing to commit (create/copy files and use "git add" to track)
+
+print('Git is awesome!')
